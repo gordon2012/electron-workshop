00f238837d647922e2a594b6c445b7d69506756e
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2017-08-31 13:02:13 -0700

    Debugging
    Signed-off-by: Gordon Doskas <gdoskas2012@email.phoenix.edu>

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const version = process.version;
-    const e = document.getElementById('info');
-    e.textContent = `I'm running Node.js version ${version}`;
+    const info = document.getElementById('info');
+    info.textContent = `I'm running Node.js version ${version}`;
+
+    const btn = document.getElementById('clickme');
+    btn.addEventListener('click', e => {
+        console.log('I was clicked.');
+        console.log(e);
+    });
 });
